6036ad50ddebf1af749d71a672611cf160ea4ce9
unknown <gibsoken@gmail.com>
2022-12-07 08:17:01 -0600

    halfway done

--- script.js
+++ script.js
@@ -6,24 +6,30 @@ var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 var special = ['!', '@', '#', '$', '%', '^', '&', '*', '+'];
 
 
+var length;
+var confirmUpperCase;
+var confirmLowerCase;
+var confirmNumbers;
+var confirmSpecial;
 
+function createPassword() {
+  window.prompt("Between 8 and 50, how many characters would you like your password to have?")
+
+  window.confirm('Would you like Capital letters in your password?');
+
+
+  window.confirm('Would you like lowercase letters in your password?')
+  
+
+  window.confirm('Would you like numbers in your password?');
+ 
+ 
+  window.confirm('Would you like special characters in your password?');
+}
 
 
 // Write password to the #password input
 function writePassword() {
-  
-  window.confirm('Would you like Capital letters in your password?');
-if (confirm(Math.random(upperCase)));
-
-  window.confirm('Would you like lowercase letters in your password?')
-  if (confirm(Math.random(lowerCase)));
-
-  window.confirm('Would you like numbers in your password?');
-  if (confirm(Math.random(numbers)));
- 
-  window.confirm('Would you like special characters in your password?');
-  if (confirm(Math.random(specail)))
-
   
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
